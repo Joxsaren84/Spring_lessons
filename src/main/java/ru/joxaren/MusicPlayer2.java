d3cb3e7fdd271d994cec00e4b2f2299ea1578a4e
Keypa <42-18@mail.ru>
2025-02-25 11:49:02 +1000

    example di with @Qualifier

--- src/main/java/ru/joxaren/MusicPlayer2.java
+++ src/main/java/ru/joxaren/MusicPlayer2.java
@@ -1,20 +1,22 @@
 package ru.joxaren;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component()
 public class MusicPlayer2 {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
+
+    private Music music1;
+    private Music music2;
 
     @Autowired
-    public MusicPlayer2(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer2(@Qualifier("rockMusicBean") Music music1, @Qualifier("classicalMusicBean")Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String playMusic(){
-        return "Playing: " + classicalMusic.getSong() + "\n" + "Playing: " + rockMusic.getSong();
+        return "Playing: \n" + music1.getSong() + "\n" + music2.getSong();
     }
 }
