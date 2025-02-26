62b176b015dd6d0d4efdb421be0e2ca62e93c6eb
Keypa <42-18@mail.ru>
2025-02-26 12:35:00 +1000

    example with annotation @Value, @Scope, @PostConstruct, @PreDestroy

--- src/main/java/ru/joxaren/MusicPlayer2.java
+++ src/main/java/ru/joxaren/MusicPlayer2.java
@@ -2,21 +2,37 @@ package ru.joxaren;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component()
 public class MusicPlayer2 {
 
+    @Value("${musicPlayer.name}")
+    private String name;
+    @Value("${musicPlayer.volume}")
+    private int volume;
+
     private Music music1;
     private Music music2;
 
     @Autowired
-    public MusicPlayer2(@Qualifier("rockMusicBean") Music music1, @Qualifier("classicalMusicBean")Music music2) {
+    public MusicPlayer2(@Qualifier("popMusicBean") Music music1, @Qualifier("classicalMusicBean")Music music2) {
         this.music1 = music1;
         this.music2 = music2;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
     public String playMusic(){
-        return "Playing: \n" + music1.getSong() + "\n" + music2.getSong();
+        return "Name: " + getName()
+                + "\nvolume: " + getVolume()
+                + "\nPlaying: \n" + music1.getSong() + "\n" + music2.getSong();
     }
 }
--- src/main/java/ru/joxaren/PopMusic.java
+++ src/main/java/ru/joxaren/PopMusic.java
@@ -1,12 +1,28 @@
 package ru.joxaren;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component("popMusicBean")
+@Scope("prototype")
 public class PopMusic implements Music{
 
     @Override
     public String getSong() {
         return "Student";
+    }
+
+    @PostConstruct
+    public void doMyInit(){
+        System.out.println("Doing my initialization.");
+    }
+
+
+    @PreDestroy
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction.");
     }
 }
